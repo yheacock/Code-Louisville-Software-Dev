102203d71abfe76cb8e6c9e693222ca1c8334163
Yaren <yarenheacock@gmail.com>
2025-10-28 01:33:38 -0400

    initial commit

---
+++ Week1/InClass/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine($"Hello user, press any key to continue...");
+
+Console.ReadKey();
+
+Console.WriteLine("Welcome to Code Louisville!\nWhat's your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("What's your pathway?");
+
+string pathway = Console.ReadLine();
+
+Console.WriteLine($"Good luck on your journey in {pathway}, {name}!");
+

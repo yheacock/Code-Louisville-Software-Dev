8753025597323002f395768d65c44b19a513c337
Yaren <yarenheacock@gmail.com>
2025-11-05 18:48:53 -0500

    StudentsForEach

---
+++ Week 3/ForEachPractice/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] student_names = { "David", "Bob", "Charlie", "John", "Mary", "Micheal"};
+
+Console.WriteLine("Students of Classroom A");
+
+foreach (string student_name in student_names){
+    Console.WriteLine(student_name);
+}
